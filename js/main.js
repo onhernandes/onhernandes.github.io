093a5f571b76aa9f52d78378cda4b7c96805890b
devmath <midia.matheus@gmail.com>
2016-07-16 13:36:48 -0300

    layout base, miss fix slide effect

--- js/main.js
+++ js/main.js
@@ -2,13 +2,25 @@ $(document).ready(function() {
 	$("#nav ul li a").click(function() {
 		var tab = $(this).attr("data-tab");
 		if (tab === "know-me") {
-			$("#wrapper section").css("transform", "translateX(0%)");
+			$(".section").css({
+				"height": "200vh",
+				"transform": "translateX(0%)"
+			});
 		} else if (tab === "projects") {
-			$("#wrapper section").css("transform", "translateX(-100%)");
+			$(".section").css({
+				"height": "150vh",
+				"transform": "translateX(-100%)"
+			});
 		} else if (tab === "talk") {
-			$("#wrapper section").css("transform", "translateX(-200%)");
+			$(".section").css({
+				"height": "100vh",
+				"transform": "translateX(-200%)"
+			});
 		} else {
-			$("#wrapper section").css("transform", "translateX(-300%)");
+			$(".section").css({
+				"height": "100vh",
+				"transform": "translateX(-300%)"
+			});
 		}
 	});
 });
