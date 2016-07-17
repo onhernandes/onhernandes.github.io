92e675bc11e7cebfa8035da8808430ca30e42154
devmath <midia.matheus@gmail.com>
2016-07-16 21:58:56 -0300

    talk added and fixed

--- js/main.js
+++ js/main.js
@@ -2,17 +2,14 @@ $(document).ready(function() {
 	$("#nav ul li a").click(function() {
 		var tab = $(this).attr("data-tab");
 		if (tab === "know-me") {
-			$("#wrapper .section").css("height", "200vh");
 			$(".section").removeClass('projects').removeClass('blog').removeClass('talk').addClass('km');
+			$("#wrapper .section").css("height", "208vh");
 		} else if (tab === "projects") {
-			$("#wrapper .section").css("height", "100vh");
 			$(".section").removeClass('blog').removeClass('km').removeClass('talk').addClass('projects');
-		} else if (tab === "talk") {
 			$("#wrapper .section").css("height", "100vh");
-			$(".section").removeClass('projects').removeClass('blog').removeClass('km').addClass('talk');
-		} else {
-			$("#wrapper .section").css("height", "100vh");
+		} else if (tab === "blog") {
 			$(".section").removeClass('projects').removeClass('km').removeClass('talk').addClass('blog');
+			$("#wrapper .section").css("height", "100vh");
 		}
 	});
 });
