c4222ec746ad636aa98e18dbf8a408be7fb7cbb5
devmath <midia.matheus@gmail.com>
2016-07-17 21:31:35 -0300

    skills bars fix

--- js/main.js
+++ js/main.js
@@ -3,13 +3,13 @@ $(document).ready(function() {
 		var tab = $(this).attr("data-tab");
 		if (tab === "know-me") {
 			$(".section").removeClass('projects').removeClass('blog').removeClass('talk').addClass('km');
-			$("#wrapper .section").css("height", "208vh");
+			//$("#wrapper .section").css("height", "208vh");
 		} else if (tab === "projects") {
 			$(".section").removeClass('blog').removeClass('km').removeClass('talk').addClass('projects');
-			$("#wrapper .section").css("height", "100vh");
+			//$("#wrapper .section").css("height", "100vh");
 		} else if (tab === "blog") {
 			$(".section").removeClass('projects').removeClass('km').removeClass('talk').addClass('blog');
-			$("#wrapper .section").css("height", "100vh");
+			//$("#wrapper .section").css("height", "100vh");
 		}
 	});
 });
