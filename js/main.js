4449d2515a4c5981e09fb76c95a3dec76c8672e3
devmath <midia.matheus@gmail.com>
2016-07-23 20:33:00 -0300

    sections changed

--- js/main.js
+++ js/main.js
@@ -2,14 +2,11 @@ $(document).ready(function() {
 	$("#nav ul li a").click(function() {
 		var tab = $(this).attr("data-tab");
 		if (tab === "know-me") {
-			$(".section").removeClass('projects').removeClass('blog').removeClass('talk').addClass('km');
-			//$("#wrapper .section").css("height", "208vh");
+			$(".section").removeClass('pj').removeClass('sk').addClass('km');
 		} else if (tab === "projects") {
-			$(".section").removeClass('blog').removeClass('km').removeClass('talk').addClass('projects');
-			//$("#wrapper .section").css("height", "100vh");
-		} else if (tab === "blog") {
-			$(".section").removeClass('projects').removeClass('km').removeClass('talk').addClass('blog');
-			//$("#wrapper .section").css("height", "100vh");
+			$(".section").removeClass('sk').removeClass('km').addClass('pj');
+		} else if (tab === "skills") {
+			$(".section").removeClass('pj').removeClass('km').addClass('sk');
 		}
 	});
 });
