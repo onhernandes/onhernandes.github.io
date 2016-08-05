50b0d27a2ce4f96fd3cce42d791bbd455099325e
devmath <midia.matheus@gmail.com>
2016-08-04 22:59:20 -0300

    add jekyll index

--- js/main.js
+++ js/main.js
@@ -1,34 +1,9 @@
 $(document).ready(function() {
-	$("#nav ul li a").click(function() {
-		var tab = $(this).attr("data-tab");
-		if (tab === "know-me") {
-			$(".section").removeClass('pj').removeClass('sk').addClass('km');
-		} else if (tab === "projects") {
-			$(".section").removeClass('sk').removeClass('km').addClass('pj');
-		} else if (tab === "skills") {
-			$(".section").removeClass('pj').removeClass('km').addClass('sk');
-		}
-	});
-
-	$(".flaticon-menu").click(function(){
-		$("nav ul").animate({
-			padding: "15px 0",
-			height: "200px"
-		}, 500);
-
-		$("nav").animate({
-			width: "100%"
-		}, 500);
-		
-		$("nav ul").click(function(){
-			$("nav ul").animate({
-				padding: "0",
-				height: "0px"
-			}, 500);
-
-			$("nav").animate({
-				width: "100px"
-			}, 500);
-		});
-	});
+	var project = $(".projects article");
+	var translate = project.css("translateX");
+	setTimeout(function() {
+		translate += 100;
+		var final = translate + "px";
+		project.css("translateX", final);
+	}, 100);
 });
