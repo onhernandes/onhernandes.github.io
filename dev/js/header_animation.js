0463e3c34059b256101444eed34323606a9103aa
mhernandes <midia.matheus@gmail.com>
2016-10-27 02:10:13 -0200

    start header animation

--- dev/js/header_animation.js
+++ dev/js/header_animation.js
@@ -1,9 +1,19 @@
 var header = document.getElementsByClassName('header'),
 	header_style = window.getComputedStyle(header[0]),
-	header_height = header_style.getPropertyValue('height');
+	header_height = header_style.getPropertyValue('height'),
+	rand = 0;
+
+function randomNum() {
+	rand = Math.floor((Math.random() * 10) + 1);
+	return this;
+}
+
+randomNum();
 
 window.addEventListener('scroll', function() {
-	header_height = header_style.getPropertyValue('height');
-
-	header[0].querySelector('.header__title').style.marginLeft = (window.pageYOffset / 5) + 'px';
+	if (rand > 1) {
+		header[0].querySelector('.header__title').style.marginLeft = (window.pageYOffset / 5) + 'px';
+	} else {
+		header[0].querySelector('.header__title').style.marginRight = (window.pageYOffset / 5) + 'px';
+	}
 });
