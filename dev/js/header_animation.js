d9eecffe770fb4c1f3c291bd2031b8a78463ed20
mhernandes <midia.matheus@gmail.com>
2016-10-27 11:37:08 -0200

    fix scss files management

--- dev/js/header_animation.js
+++ dev/js/header_animation.js
@@ -13,20 +13,17 @@ randomNum();
 window.addEventListener('scroll', function() {
 	if (rand < 3) {
 		if (window_width > 960) {
-			//left
 			header[0].querySelector('.header__title').style.transform = 'translateX(' + (window.pageYOffset / 5) + 'px)';
 		} else {
 			header[0].querySelector('.header__title').style.transform = 'translateX(' + (window.pageYOffset / 1.5) + 'px)';
 		}
 	} else if (rand < 6) {
-			//top
 		if (window_width > 960) {
 			header[0].querySelector('.header__title').style.transform = 'translateY(' + (window.pageYOffset / 5) + 'px)';
 		} else {
 			header[0].querySelector('.header__title').style.transform = 'translateY(' + (window.pageYOffset) + 'px)';
 		}
 	} else {
-			//right
 		if (window_width > 960) {
 			header[0].querySelector('.header__title').style.transform = 'translateX(-' + (window.pageYOffset / 5) + 'px)';
 		} else {
