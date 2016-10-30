94b0f39f39c982c62d76834693debcd27bb4dc70
mhernandes <midia.matheus@gmail.com>
2016-10-30 12:59:50 -0200

    fix header animation

--- dev/js/header_animation.js
+++ dev/js/header_animation.js
@@ -3,25 +3,13 @@ var header = document.getElementsByClassName('header');
 if (header.length > 0 && window_width > 960) {
 	window.addEventListener('scroll', function() {
 		if (rand < 3) {
-			if (window_width > 960) {
-				header[0].querySelector('.header__title').style.transform = 'translateX(' + (window.pageYOffset / 5) + 'px)';
-			} else {
-				header[0].querySelector('.header__title').style.transform = 'translateX(' + (window.pageYOffset / 1.5) + 'px)';
-			}
+			header[0].querySelector('.header__title').style.transform = 'translateX(' + (window.pageYOffset / 5) + 'px)';
 		} else if (rand < 6) {
-			if (window_width > 960) {
-				header[0].querySelector('.header__title').style.transform = 'translateY(' + (window.pageYOffset / 5) + 'px)';
-			} else {
-				header[0].querySelector('.header__title').style.transform = 'translateY(' + (window.pageYOffset) + 'px)';
-			}
+			header[0].querySelector('.header__title').style.transform = 'translateY(' + (window.pageYOffset / 2) + 'px)';
 		} else {
-			if (window_width > 960) {
-				header[0].querySelector('.header__title').style.transform = 'translateX(-' + (window.pageYOffset / 5) + 'px)';
-			} else {
-				header[0].querySelector('.header__title').style.transform = 'translateX(-' + (window.pageYOffset / 1.5) + 'px)';
-			}
+			header[0].querySelector('.header__title').style.transform = 'translateX(-' + (window.pageYOffset / 5) + 'px)';
 		}
 
-		header[0].querySelector('.header__background').style.transform = 'translateY(' + (window.pageYOffset / 2) + 'px)';
+		header[0].querySelector('.header__background').style.transform = 'translateY(' + (window.pageYOffset / 10) + 'px)';
 	});
 }
