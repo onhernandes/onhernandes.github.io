27b1082e70d41be40ec86ac95bf527f9d4aeb007
mhernandes <midia.matheus@gmail.com>
2016-11-07 16:22:31 -0200

    fix article content animation

--- dev/js/article_content.js
+++ dev/js/article_content.js
@@ -4,7 +4,7 @@ if (article_section.length > 0 && window_width > 960) {
 	var article_each = article_section[0].getElementsByTagName('article');
 
 	window.addEventListener('scroll', function() {
-		if ((window.pageYOffset / 4) < 100) {
+		if ((window.pageYOffset / 4) < 150) {
 			for (var i = 0; i < article_each.length; i++) {
 				article_each[i].style.transform = 'translateY(-' + (window.pageYOffset / 4) + 'px)';
 			}
