214580c762b70a0334fcb49d1914ab974f2f21da
Matheus Hernandes <midia.matheus@gmail.com>
2017-01-25 21:37:06 -0200

    fix menu structure

--- dev/js/menu.js
+++ dev/js/menu.js
@@ -1,15 +1,20 @@
 var menu = document.getElementsByClassName('menu'),
-	menu_height = menu[0].clientHeight,
-	header = document.getElementsByClassName('header'),
-	header_height = header[0].clientHeight;
+	menu__icon = menu[0].childNodes[1],
+	menu__search = menu[0].childNodes[3],
+	menu__list = menu[0].childNodes[5];
 
-menu[0].style.height = menu_height + 'px';
+//menu__list.style.marginLeft = '-' + (menu__list.clientWidth + 10) + 'px';
 
 if (window_width > 960) {
-	window.addEventListener('scroll', function() {
-		menu_height = menu[0].clientHeight;
-		console.log(menu_height);
+	menu__icon.addEventListener('click', function() {
+		if (menu__list.className.indexOf('active') !== -1) {
+			menu__list.className = 'menu__list';
+		} else {
+			menu__list.className += ' active';
+		}
+	});
 
-		menu[0].style.height = (menu_height - (window.pageYOffset / 2)) + 'px';
+	menu__icon.addEventListener('mouseout', function() {
+		//menu__list.style.marginLeft = '-' + (menu__list.clientWidth + 10) + 'px';
 	});
 }
