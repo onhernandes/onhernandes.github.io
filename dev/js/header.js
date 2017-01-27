6ad6ef2b38ec5b3f2c91e3d9b937fe408d52012a
Matheus Hernandes <midia.matheus@gmail.com>
2017-01-26 23:42:54 -0200

    fix header

---
+++ dev/js/header.js
@@ -0,0 +1,7 @@
+var header = document.getElementsByClassName('header'),
+	header__wrapper = header[0].querySelector('.header__wrapper');
+
+
+window.addEventListener('scroll', function() {
+	header__wrapper.style.paddingTop = (window.pageYOffset / 2) + 'px';
+});
