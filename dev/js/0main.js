8a6c32d97ec7bb3c62de16c3708d6ea32ec3d160
Matheus Hernandes <midia.matheus@gmail.com>
2017-02-04 19:53:09 -0200

    add projects_data

--- dev/js/0main.js
+++ dev/js/0main.js
@@ -1,5 +1,12 @@
 var window_width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth,
-	body = document.getElementsByTagName('body');
+	body = document.getElementsByTagName('body'),
+	projects_data = [
+		{
+			name: 'teste',
+			desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae, natus!',
+			github: 'https://github.com/mhernandes'
+		}
+	];
 
 function lockBody() {
 	body[0].style.overflow = 'hidden';
