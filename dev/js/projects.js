7a8ca5dd0d41beb6b292c3e015d615c731a44077
Matheus Hernandes <midia.matheus@gmail.com>
2017-02-05 13:55:41 -0200

    fix project home section

--- dev/js/projects.js
+++ dev/js/projects.js
@@ -1,15 +1,71 @@
-if (checkPage('home')) {
-	var projects = document.getElementsByClassName('projects'),
-		projects__item = projects[0].getElementsByClassName('projects__item'),
+if (checkPage('home') || checkPage('about')) {
+	var projects = document.getElementsByClassName('projects');
+	
+	projects_data.forEach(function(obj, index) {
+		var	article = document.createElement('article'),
+			title = document.createElement('a'),
+			title_h1 = document.createElement('h1'),
+			intro = document.createElement('a'),
+			list = document.createElement('ul'),
+			list_li = (obj.link !== false ? new Array() : document.createElement('li')),
+			list_li_a = document.createElement('a');
+
+		title_h1.textContent = obj.name;
+		title.appendChild(title_h1);
+		title.setAttribute('href', obj.github);
+		title.setAttribute('target', '_blank');
+		title.className = 'projects__item_title';
+
+		intro.textContent = obj.desc;
+		intro.setAttribute('href', obj.github);
+		intro.setAttribute('target', '_blank');
+		intro.className = 'projects__item_intro';
+
+		if (list_li.constructor === Array) {
+			list_li[0] = document.createElement('li');
+			list_li_a.setAttribute('href', obj.github);
+			list_li_a.setAttribute('target', '_blank');
+			list_li_a.setAttribute('icon', 'github');
+			list_li[0].appendChild(list_li_a);
+
+			list_li[1] = document.createElement('li');
+			list_li_a = document.createElement('a');
+			list_li_a.setAttribute('href', obj.link);
+			list_li_a.setAttribute('target', '_blank');
+			list_li_a.setAttribute('icon', 'link');
+			list_li[1].appendChild(list_li_a);
+
+			list.appendChild(list_li[0]);
+			list.appendChild(list_li[1]);
+		} else {
+			list_li = document.createElement('li');
+			list_li_a.setAttribute('href', obj.github);
+			list_li_a.setAttribute('target', '_blank');
+			list_li_a.setAttribute('icon', 'github');
+			list_li.appendChild(list_li_a);
+			list.appendChild(list_li);
+		}
+
+		article.appendChild(title);
+		article.appendChild(intro);
+		article.appendChild(list);
+		article.className = 'projects__item';
+
+		projects[0].appendChild(article);
+	});
+
+
+	var	projects__item = projects[0].getElementsByClassName('projects__item'),
 		item_width = projects__item[0].clientWidth,
 		new_width = total_width = item_counter = displayed = 0,
 		total_items = projects__item.length;
 
-	// Get the width from all of the items 
+	// Get the width from all of the items
 	for (var i = 0; i < projects__item.length; i++) {
 		total_width += projects__item[i].clientWidth;
 	}
 
+	// Set how many items are being displayed
 	if (window_width > 1280) {
 		displayed = 4;
 	} else if (window_width > 760) {
