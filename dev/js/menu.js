0074c26e5234e6a506b13720dde2992624fe54e3
Matheus Hernandes <midia.matheus@gmail.com>
2017-02-10 10:45:18 -0200

    fix menu again

--- dev/js/menu.js
+++ dev/js/menu.js
@@ -1,8 +1,8 @@
 var menu = document.getElementsByClassName('menu'),
 	menu__icon = menu[0].childNodes[1],
 	menu__search = menu[0].childNodes[3],
-	menu__wrapper = menu[0].childNodes[5],
-	menu__close = menu__wrapper.querySelector('.menu__wrapper_close'),
+	menu__wrapper = document.getElementsByClassName('menu__wrapper'),
+	menu__close = document.getElementsByClassName('menu__wrapper_close'),
 	search = document.getElementsByClassName('search'),
 	search_title = document.getElementsByClassName('search__form_title'),
 	close_search = search_title[0].childNodes[1];
@@ -15,18 +15,24 @@ menu__icon.addEventListener('click', function() {
 		menu__wrapper.className += ' active';
 	}*/
 
-	if (menu__wrapper.className.indexOf('active') == -1) {
-		menu__wrapper.className += ' active';
+	if (menu__wrapper[0].className.indexOf('active') == -1) {
+		menu__wrapper[0].className += ' active';
 	}
 });
 
 if (window_width < 760) {
-	menu__close = menu__wrapper.childNodes[1].childNodes[1];
+	menu__close[0] = menu__wrapper[0].childNodes[1].childNodes[1];
 }
 
-menu__close.addEventListener('click', function() {
-	menu__wrapper.className = 'menu__wrapper';	
+menu__close[0].addEventListener('click', function() {
+	menu__wrapper[0].className = 'menu__wrapper';	
 });
+
+if (window_width < 760) {
+	document.getElementsByClassName('menu__wrapper_close-mobile')[0].addEventListener('click', function() {
+		menu__wrapper[0].className = 'menu__wrapper';	
+	});
+}
 
 menu__search.addEventListener('click', function() {
 	search[0].style.marginTop = 0;
