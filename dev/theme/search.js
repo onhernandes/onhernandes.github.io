6d41d5fbf4a1828c73f426254a11d4b8b1808486
Matheus Hernandes <midia.matheus@gmail.com>
2017-02-18 14:22:18 -0200

    fix things

--- dev/theme/search.js
+++
@@ -1,7 +0,0 @@
-/*SimpleJekyllSearch({
-	searchInput: document.getElementById('search_input'),
-	resultsContainer: document.getElementById('search_results'),
-	json: '/search.json',
-	searchResultTemplate: '<li><a href="{url}">{title}</a></li>',
-	noResultsText: '<span>Ops! Anything was found, try again!</span>'
-});*/
