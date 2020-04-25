14d586caad3bd1ec4551f86908c4efb4a35d205f
Matheus Hernandes <midia.matheus@gmail.com>
2020-04-25 19:51:23 -0300

    fix

--- publish-to-gh-pages.sh
+++ publish-to-gh-pages.sh
@@ -1,2 +1,2 @@
 hugo
-cd public && git add --all && git commit -m "Publishing to gh-pages" && git push origin gh-pages && cd ..
+cd public && git add --all && git commit -m "Publishing to gh-pages" && git push origin master && cd ..
