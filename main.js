23fdba387a92587990e0b808bd832ff1fd74e104
Matheus Hernandes <12698656+onhernandes@users.noreply.github.com>
2020-08-27 12:49:52 -0300

    Publishing to gh-pages

---
+++ main.js
@@ -0,0 +1,19 @@
+const isRunningLocal = href => href.match(/localhost:1313/g) !== null
+const isRunningElsewhere = href => href.match(/onhernandes\.github\.io/) === null
+
+document.onload = () => {
+  document
+    .querySelectorAll('a')
+    .forEach(link => {
+      const href = link.getAttribute('href')
+      const url = new URL(href)
+
+      const isLocalhost = isRunningLocal(url.origin)
+      const isElsewhere = isRunningElsewhere(url.origin)
+      const hasTarget = link.hasAttribute('target')
+
+      if (!isLocalhost && isElsewhere && !hasTarget) {
+        link.setAttribute('target', '_blank')
+      }
+    })
+}
